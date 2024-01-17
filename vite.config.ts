3e35c3e197342dd9b6f35546ed7604203a475ea0
Marcos Hernández <marcoshdezcam@gmail.com>
2024-01-16 19:00:08 -0600

    Setup Vite aliases.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      '~bootstrap-icons': path.resolve(__dirname, 'node_modules/bootstrap-icons'),
+      '~socicon': path.resolve(__dirname, 'node_modules/socicon'),
+      '~@fortawesome': path.resolve(__dirname, 'node_modules/@fortawesome'),
+      '~line-awesome': path.resolve(__dirname, 'node_modules/line-awesome'),
+      '~prism-themes': path.resolve(__dirname, 'node_modules/prism-themes'),
+      '~animate.css': path.resolve(__dirname, 'node_modules/animate.css'),
+      '~nouislider': path.resolve(__dirname, 'node_modules/nouislider'),
+    }
+  }
 })
